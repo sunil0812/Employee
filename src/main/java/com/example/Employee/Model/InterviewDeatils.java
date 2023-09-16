ddbb8aeefb9be418a22c15a93e529a80a49ecd69
sunil0812 <sunilkumarack2@gmail.com>
2023-09-16 07:52:28 +0530

    New Project Staffing

---
+++ src/main/java/com/example/Employee/Model/InterviewDeatils.java
@@ -0,0 +1,18 @@
+package com.example.Employee.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class InterviewDeatils {
+
+    private String mode;
+    private String link;
+    private String status;
+    private String locationDetails;
+
+
+}
